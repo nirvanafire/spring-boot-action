de1303bff49b8dc1b14bdf73d4195206ddbbae73
nirvanafire <nirvanafire@gmail.com>
2025-06-20 09:58:54 +0800

    first commit

---
+++ src/test/java/xin/keep/actioin/SpringBootActionApplicationTests.java
@@ -0,0 +1,13 @@
+package xin.keep.actioin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
